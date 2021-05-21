RekCuy63/composer

commit dd54f295fe859dd76e330994d54a86570f0dc89d

Fix progress output, and remove progress bar on CI env, fixes #9621, closes #9880

src/Composer/Installer/InstallationManager.php
@@ -451,12 +451,22 @@ class InstallationManager
     private function waitOnPromises(array $promises)
     {
         $progress = null;
-        if ($this->outputProgress && $this->io instanceof ConsoleIO && !$this->io->isDebug() && count($promises) > 1) {
+        if (
+            $this->outputProgress
+            && $this->io instanceof ConsoleIO
+            && !getenv('CI')
+            && !$this->io->isDebug()
+            && count($promises) > 1
+        ) {
             $progress = $this->io->getProgressBar();
         }
         $this->loop->wait($promises, $progress);
         if ($progress) {
             $progress->clear();
+            // ProgressBar in non-decorated output does not output a final line-break and clear() does nothing
+            if ($this->io->isDecorated()) {
+                $this->io->writeError('');
+            }
         }
     }
 

src/Composer/Util/Loop.php
@@ -108,7 +108,7 @@ class Loop
 
         // as we skip progress updates if they are too quick, make sure we do one last one here at 100%
         if ($progress) {
-            $progress->setProgress($progress->getMaxSteps());
+            $progress->finish();
         }
 
         unset($this->currentPromises[$waitIndex]);

commit 00b83c475e60db6cde6a27b99533917e31fcf999

Fix regression introduced in dd54f295fe859dd76e330994d54a86570f0dc89d

src/Composer/Installer/InstallationManager.php
@@ -464,7 +464,7 @@ class InstallationManager
         if ($progress) {
             $progress->clear();
             // ProgressBar in non-decorated output does not output a final line-break and clear() does nothing
-            if ($this->io->isDecorated()) {
+            if (!$this->io->isDecorated()) {
                 $this->io->writeError('');
             }
         }